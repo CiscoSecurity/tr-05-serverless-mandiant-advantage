3c2d5adf0e9a67cdc0a2e2c8ad51cd2eb745e298
ceilingduster <iaredden@cisco.com>
2023-08-07 22:52:48 +0000

    first commit

---
+++ code/config.py
@@ -0,0 +1,22 @@
+import json
+from uuid import NAMESPACE_X500
+
+
+class Config:
+    settings = json.load(open('container_settings.json', 'r'))
+    VERSION = settings["VERSION"]
+
+    API_URL = 'https://api.intelligence.mandiant.com/'
+    UI_URL = 'https://advantage.mandiant.com/'
+
+    USER_AGENT = ('SecureX Threat Response Integrations '
+                  '<tr-integrations-support@cisco.com>')
+
+    CTR_ENTITIES_LIMIT_MAX = 1000
+    CTR_ENTITIES_LIMIT_DEFAULT = 100
+
+    NUMBER_OF_DAYS_VERDICT_IS_VALID = 30
+    NUMBER_OF_DAYS_JUDGEMENT_IS_VALID = 7
+    NUMBER_OF_DAYS_INDICATOR_IS_VALID = 30
+
+    NAMESPACE_BASE = NAMESPACE_X500
