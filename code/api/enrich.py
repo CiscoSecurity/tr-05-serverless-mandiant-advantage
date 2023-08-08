2a18795a5ff48f5f401c8fb6f2a7c515c5f76918
ceilingduster <iaredden@cisco.com>
2023-08-07 19:12:46 -0700

    working relay

--- code/api/enrich.py
+++ code/api/enrich.py
@@ -1,14 +1,11 @@
 from functools import partial
 
-from api.client import TemplateClient
 from api.mapping import Sighting
 from api.schemas import ObservableSchema
 from api.utils import (
-    filter_observables,
-    get_credentials,
     get_json,
+    jsonify_errors,
     jsonify_data,
-    jsonify_result,
 )
 from flask import Blueprint, current_app, g
 
@@ -27,23 +24,8 @@ def deliberate_observables():
 @enrich_api.route("/observe/observables", methods=["POST"])
 def observe_observables():
     _ = get_credentials()
-    observables = filter_observables(get_observables())
-
-    g.sightings = []
-    limit = current_app.config["CTR_ENTITIES_LIMIT"]
-    client = TemplateClient()
-
-    for observable in observables:
-        threats = client.get_threats()
-        sighting_mapping = Sighting(observable)
-
-        if len(threats) > limit:
-            threats = threats[:limit]
-        for threat in threats:
-            mapped_sighting = sighting_mapping.extract(threat)
-            g.sightings.append(mapped_sighting)
-
-    return jsonify_result()
+    _ = get_observables()
+    return jsonify_data({})
 
 
 @enrich_api.route("/refer/observables", methods=["POST"])
